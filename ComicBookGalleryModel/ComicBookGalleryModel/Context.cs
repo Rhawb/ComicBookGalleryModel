5f22141fe4b0bc13d8ddc5c215fd6d91be9cc00e
Rob <tehrhawb@gmail.com>
2017-07-25 18:01:25 -0400

    Updates.

--- ComicBookGalleryModel/ComicBookGalleryModel/Context.cs
+++ ComicBookGalleryModel/ComicBookGalleryModel/Context.cs
@@ -10,6 +10,12 @@ namespace ComicBookGalleryModel
 {
 	public class Context : DbContext
 	{
+		public Context()
+		{
+			Database.SetInitializer(new DropCreateDatabaseAlways<Context>());
+		}
+
 		public DbSet<ComicBook> ComicBooks { get; set; }
+
 	}
 }
